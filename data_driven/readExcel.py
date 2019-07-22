98baee6b8c53eb85ba141d393d97182ac26ebb37
snehakpersistent <sneha_kanekar@persistent.com>
2019-07-22 11:29:48 +0530

    Create readExcel.py

---
+++ data_driven/readExcel.py
@@ -0,0 +1,18 @@
+import openpyxl
+
+path="C:\\Users\sneha_kanekar\PycharmProjects\SeleniumwithPython\data.xlsx"
+
+workbook=openpyxl.load_workbook(path)
+
+sheet=workbook.active
+
+rows=sheet.max_row
+cols=sheet.max_column
+
+print(rows)
+print(cols)
+
+for r in range(1,rows+1):
+    for c in range(1,cols+1):
+        print(sheet.cell(row=r,column=c).value,end="     ")
+    print()
